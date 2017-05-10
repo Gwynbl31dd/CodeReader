81f789ee07db2bd110ada70e0701fb20847d9901
root <root@LAPTOP-99HME2HQ.BigPond>
2017-05-10 10:30:18 +1000

    Initial commit

---
+++ app/src/main/java/com/clone_soft/paulin/codereader/MainActivity.java
@@ -0,0 +1,13 @@
+package com.clone_soft.paulin.codereader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
